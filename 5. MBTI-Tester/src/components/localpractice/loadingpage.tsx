ee510baaef0ea651a5f5b220cae5b268450cd4fc
gomao <gboycdw@gmail.com>
2023-05-18 20:10:36 +0900

    결과 창 버튼클릭 없이 자동으로 진행, 오리 걸어가게 변경

--- 5. MBTI-Tester/src/components/localpractice/loadingpage.tsx
+++ 5. MBTI-Tester/src/components/localpractice/loadingpage.tsx
@@ -41,22 +41,26 @@ function LoadingPage(props: Props) {
     calculate += "P";
   }
   let imgLink = require("../../images/gogo.gif");
+  setTimeout(() => {
+    props.setMBTI(calculate);
+    navigate("/result");
+  }, 4000);
+
   return (
     <Box>
       <div>
         <div
           style={{
-            display: "flex",
             alignItems: "center",
             justifyContent: "center",
           }}
         >
-          <div style={{ fontSize: 13, marginRight: 3, paddingRight: 22 }}>
-            검사가 완료되었습니다. 버튼을 놀러 결과를 확인하세요.
-          </div>
+          <ResultTitle>
+            검사가 완료되었습니다. 잠시 후 결과 페이지로 이동합니다.
+          </ResultTitle>
           <Image src={imgLink}></Image>
         </div>
-        <button
+        {/* <button
           style={{
             display: "flex",
             justifyContent: "flex-end",
@@ -64,13 +68,11 @@ function LoadingPage(props: Props) {
             marginRight: 27,
           }}
           onClick={() => {
-            props.setMBTI(calculate);
-            navigate("/result");
-            console.log(props.MBTI);
+
           }}
         >
           결과 확인하기
-        </button>
+        </button> */}
       </div>
     </Box>
   );
@@ -91,7 +93,7 @@ const Box = styled.div`
   font-family: "Poppins", sans-serif;
   // align-items: center;
   // justify-content: center;
-  background-color: beige;
+  background-color: white;
 `;
 
 const ResultTitle = styled.div`
@@ -100,33 +102,21 @@ const ResultTitle = styled.div`
   justify-content: center;
   align-items: center;
   margin: 5px 5px;
-  font-size: 25px;
-`;
-const ResultTitleText = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 5px 5px;
-`;
-
-const ResultList = styled.li`
-  display: flex;
-  align-items: center;
   font-size: 15px;
-`;
-
-const ResultBody = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid;
-  border-radius: 5px;
-  padding: 7px 7px;
-  font-size: 13px;
-  margin: 5px 5px;
 `;
 
 const Image = styled.img`
   width: 120px;
   height: auto;
+  margin-top: 20px;
+
+  animation: slideLeft 4s ease-in-out none;
+  @keyframes slideLeft {
+    0% {
+      transform: translateX(400px);
+    }
+    100% {
+      transform: translateX(0);
+    }
+  }
 `;
